b82374de59171947d5f98d36e08dc64439d03872
Dominae Ponds <dominae1@graceland.edu>
2018-03-19 12:49:00 -0500

    website update

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Hello, welcome to my page! ")
+var name = "Doma";
+document.write ("<span id = 'footer'>Doma's Website</span><br />");
+document.write ("<p>&copy; 2018 Your Name <br />");
+document.write ("Modified February 26 2018 <br />");
+document.write ("Retrieved: Monday Mar 05 2018 12:36:02 GMT-0600 (Central Standard Time)");
